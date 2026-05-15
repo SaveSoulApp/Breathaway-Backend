10e3a42daedaba8be66561a0c28312ad56ca7142
Mohit Malpani <androidwithoutwifi@gmail.com>
2026-05-16 01:45:29 +0530

    Delete unwanted decorators

--- src/common/decorators/client-id.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const ClientId = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.clientId; // This was set by the middleware
-  },
-);
