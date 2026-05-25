42b6b8616691cdc45eb1c7f1ab59310d229dd8a5
Mohit Malpani <androidwithoutwifi@gmail.com>
2026-05-25 23:52:50 +0530

    Add call to push after instagram verification

--- src/modules/identity-workflows/identity-workflows.module.ts
+++ src/modules/identity-workflows/identity-workflows.module.ts
@@ -1,4 +1,5 @@
 import { IdentityModule } from '@modules/identities/identities.module';
+import { NotificationsModule } from '@modules/notifications/notifications.module';
 import { OtpModule } from '@modules/one-time-passwords/one-time-passwords.module';
 import { SocialidentityModule } from '@modules/social-identities/social-identities.module';
 import { Module } from '@nestjs/common';
@@ -6,7 +7,12 @@ import { IdentityWorkflowsController } from './identity-workflows.controller';
 import { IdentityWorkflowsService } from './identity-workflows.service';
 
 @Module({
-  imports: [OtpModule, SocialidentityModule, IdentityModule],
+  imports: [
+    OtpModule,
+    SocialidentityModule,
+    IdentityModule,
+    NotificationsModule,
+  ],
   controllers: [IdentityWorkflowsController],
   providers: [IdentityWorkflowsService],
 })
