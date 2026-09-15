9a5142fce428b36668b5b1f41c7f8e56e974afd2
Mohit Malpani <androidwithoutwifi@gmail.com>
2026-09-15 08:03:35 +0530

    Add joi for validation of env params

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
-import { TimezoneResponseInterceptor } from './common/interceptors';
 import { randomUUID } from 'crypto';
-import { ClientIdentityGuard } from '@common/guards/client-identity.guard';
+
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
@@ -9,10 +8,18 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { Request } from 'express';
 import { ClsModule } from 'nestjs-cls';
+
+import { ClientIdentityGuard } from '@common/guards/client-identity.guard';
 import { AppValidationPipe } from '@core/pipes';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TimezoneResponseInterceptor } from './common/interceptors';
 import { configureMiddleware, MiddlewareModule } from './common/middlewares';
+import {
+  envValidationOptions,
+  envValidationSchema,
+} from './config/env.validation';
 import { GcpSecretManagerModule } from './core/gcp-secret-manager/gcp-secret-manager.module';
 import { LoggerModule } from './core/logger';
 import { PrismaModule } from './infrastructure/database/prisma.module';
@@ -20,6 +27,7 @@ import { AdminModule } from './modules/admin/admin.module';
 import { AuditModule } from './modules/audit/audit.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { BlocksModule } from './modules/blocks/blocks.module';
+import { ChatsModule } from './modules/chats/chats.module';
 import { CreditsModule } from './modules/credits/credits.module';
 import { DevicesModule } from './modules/devices/devices.module';
 import { FirebaseModule } from './modules/firebase/firebase.module';
@@ -36,12 +44,11 @@ import { OneTimePasswordsModule } from './modules/one-time-passwords/one-time-pa
 import { PreferencesModule } from './modules/preferences/preferences.module';
 import { ProfilesModule } from './modules/profiles/profiles.module';
 import { PubSubModule } from './modules/pubsub/pubsub.module';
+import { ReportsModule } from './modules/reports/reports.module';
 import { SocialIdentitiesModule } from './modules/social-identities/social-identities.module';
 import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { WebhooksModule } from './modules/webhooks/webhooks.module';
-import { ChatsModule } from './modules/chats/chats.module';
-import { ReportsModule } from './modules/reports/reports.module';
 
 @Module({
   imports: [
@@ -49,6 +56,8 @@ import { ReportsModule } from './modules/reports/reports.module';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
       cache: true,
+      validationSchema: envValidationSchema,
+      validationOptions: envValidationOptions,
     }),
     ClsModule.forRoot({
       global: true,
